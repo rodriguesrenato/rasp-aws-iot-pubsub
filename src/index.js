0e89ae369a4e36954dd7355006d815ae47f25f87
rodriguesrenato <renato.rodrigues.rfr@gmail.com>
2020-11-22 21:22:37 -0300

    feat:Added funciton to pub system status on fixed interval

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 var awsIot = require('aws-iot-device-sdk');
 const dotenv = require('dotenv').config();
 
+// Load connection configurations from .env
 configs_aws_iot = {
     accessKeyId: process.env.AWS_IOT_KEY,
     secretAccessKey: process.env.AWS_IOT_SECRET,
@@ -12,54 +13,73 @@ configs_aws_iot = {
     region: process.env.AWS_IOT_REGION,
     host: process.env.AWS_IOT_ENDPOINT,
     keepalive: parseInt(process.env.AWS_IOT_KEEPALIVE),
-  }
+    statusTopic: process.env.ROBOT_STATUS_TOPIC,
+    intervalStatus: parseInt(process.env.ROBOT_INTERVAL_STATUS),
+}
 
+// configure list of topics to be subscribed
+var sub_topic_list = [configs_aws_iot.clientId+'/logs'];
 
-var topic_list = [configs_aws_iot.clientId];
 //setup paths to certificates
 var device = awsIot.device(configs_aws_iot);
 
 device
     .on('connect', function () {
-        console.log('connect');
-        for (var i = 0; i < topic_list.length; i++) {
-            device.subscribe(topic_list[i]);
-            console.log("subscribed to topic: "+topic_list[0])
+        console.log('>connect');
+        for (var i = 0; i < sub_topic_list.length; i++) {
+            device.subscribe(sub_topic_list[i]);
+            console.log("subscribed to topic: " + sub_topic_list[0])
         }
 
         // publish a startup log message
         var message = {
-            client_id:configs_aws_iot.clientId,
-            status:"connected"
+            client_id: configs_aws_iot.clientId,
+            status: "connected"
         }
-        device.publish(topic_list[0], JSON.stringify({
+        device.publish(sub_topic_list[0], JSON.stringify({
             message
-         }));    
+        }));
     });
 
 device
     .on('message', function (topic, payload) {
         // convert the payload to a JSON object
+        console.log('>message');
         var payload = JSON.parse(payload.toString());
         console.log(payload);
     });
 device
     .on('close', function () {
-        console.log('close');
+        console.log('>close');
     });
 device
     .on('end', function () {
-        console.log('end');
+        console.log('>end');
     });
 device
     .on('reconnect', function () {
-        console.log('reconnect');
+        console.log('>reconnect');
     });
 device
     .on('offline', function () {
-        console.log('offline');
+        console.log('>offline');
     });
 device
     .on('error', function (error) {
-        console.log('error', error);
+        console.log('>error', error);
     });
+
+function getSystemParams() {
+    console.log('>>getSystemParams');
+
+    var message = {
+        client_id: configs_aws_iot.clientId,
+        status: "robot_status"
+    }
+    device.publish(configs_aws_iot.clientId+'/status', JSON.stringify({
+        message
+    }));
+
+}
+
+setInterval(getSystemParams, intervalStatus);
